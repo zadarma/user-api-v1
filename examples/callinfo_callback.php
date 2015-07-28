c21376dc47583dd8af3539f5709541814cf901a7
Zadarma <github@zadarma.com>
2015-07-28 17:34:04 +0300

    Added callinfo link example

---
+++ examples/callinfo_callback.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_GET['zd_echo'])) exit($_GET['zd_echo']);
+
+if ($_POST) {
+    $callerId = $_POST['caller_id']; // number of calling party;
+    $calledDid = $_POST['called_did']; // number of called party;
+    $callstart = $_POST['callstart']; // start time of call
+
+    /*
+    // For each request you can easily change work scenario for current call by sending in response on information:
+    echo json_encode(array(
+        'redirect' => 1,
+        'caller_name' => 'TestName'
+    ));
+    exit();
+    */
+}

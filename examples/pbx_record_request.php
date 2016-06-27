460fbd9ad6c79a2f1a78670b884174bd5881c563
Zadarma <github@zadarma.com>
2016-06-27 13:58:42 +0300

    Update PBX records request by pbx_call_id

--- examples/pbx_record_request.php
+++ examples/pbx_record_request.php
@@ -3,8 +3,9 @@
 include_once 'include.php';
 
 $params = array(
-    'call_id' => '11111111111111.1111',
-//    'lifetime' => 12000,
+    'call_id' => '1458832388.1585217',
+//    'pbx_call_id' => 'in_c2b77d043ae465a1072d3f745e071032b9485461',
+//    'lifetime' => 180,
 );
 
 $zd = new \Zadarma_API\Client(KEY, SECRET);
@@ -13,7 +14,9 @@ $answer = $zd->call('/v1/pbx/record/request/', $params);
 $answerObject = json_decode($answer);
 
 if ($answerObject->status == 'success') {
+    echo "<pre>";
     print_r($answerObject);
+    echo "</pre>";
 } else {
     echo $answerObject->message;
 }

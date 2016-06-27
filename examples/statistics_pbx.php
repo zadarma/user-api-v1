3d169499cc91c1717fa59735b2b96604a3f2f070
Zadarma <github@zadarma.com>
2016-06-27 13:49:45 +0300

    Update PBX statistics

--- examples/statistics_pbx.php
+++ examples/statistics_pbx.php
@@ -3,13 +3,13 @@
 include_once 'include.php';
 
 $params = array(
-    'start' => '2015-01-01 00:00:00',
-    'end' => '2015-01-31 00:00:00'
+    'start' => '2016-01-01 00:00:00',
+    'end' => '2016-01-31 00:00:00',
+//    'version' => 2
 );
 
 $zd = new \Zadarma_API\Client(KEY, SECRET);
 $answer = $zd->call('/v1/statistics/pbx/', $params);
-
 $answerObject = json_decode($answer);
 
 if ($answerObject->status == 'success') {
